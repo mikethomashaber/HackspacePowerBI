160fc318b159b500705cc9460437cb4abda210d6
Mike Haber <mike.haber@ntlworld.com>
2024-08-07 13:34:39 +0100

    Improved Comments
    Improved comments, and removed code to write to csv that was commented out.

--- get hms membership data conversions in dataframe.py
+++ get hms membership data conversions in dataframe.py
@@ -4,19 +4,16 @@ import lxml
 import html5lib 
 from bs4 import BeautifulSoup
 
+#This script gets data from a page on HMS that is available without logging in. The page has membership metrics including statistics
+#Another script gets membership data perday direct from cacti, and this may be a better source. Power BI can create the stats as required.
+
 url = 'https://hms.nottinghack.org.uk/statistics/membership'
 html = requests.get(url).content
 df_list = pd.read_html(html)
-#filename = "membership.csv"
-#file1 = open(filename, "w", encoding="utf-8")  # write mode
-
 
 df0 = df_list[0]
 df1 = df_list[1]
 df3 = df_list[0] + df_list[1]
 
-
-
 print(df1)
 
-#file1.write(data1 + data2)
--- get hms membership data visitors in dataframe.py
+++ get hms membership data visitors in dataframe.py
@@ -4,6 +4,10 @@ import lxml
 import html5lib 
 from bs4 import BeautifulSoup
 
+#This script gets data from the membership page of HMS. This data has alreday been processed
+#getting the data direct from cacti may be a better idea.
+#powerbi can use the cacti api endpoints directly as a web import
+
 url = 'https://hms.nottinghack.org.uk/statistics/membership'
 html = requests.get(url).content
 df_list = pd.read_html(html)
